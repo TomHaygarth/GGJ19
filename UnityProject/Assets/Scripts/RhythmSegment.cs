a7a2ea0511df043cbf8a4e88d6dc8d5b5b76fb53
Thomas Haygarth <haygarth.thomas@gmail.com>
2019-01-25 22:30:47 +0000

    Added a simple scriptable object to represent a track segment and obstacles

---
+++ UnityProject/Assets/Scripts/RhythmSegment.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenuAttribute(menuName="create/rhythm segment")]
+public class RhythmSegment : ScriptableObject {
+
+	public enum ObstacleType
+	{
+		None,
+		Left_Side,
+		Right_Side,
+		Both_Sides
+	};
+
+	[SerializeField]
+	private AudioClip m_track;
+
+	[SerializeField]
+	private int m_BPM = 125;
+
+	[SerializeField]
+	private ObstacleType[] m_beatObstacles;
+
+	public int BPM { get { return m_BPM; } }
+	public AudioClip Track { get { return m_track; } }
+	public ObstacleType BeatObstacles { get { return m_track; } }
+
+
+}
