bf55b1ba8c5ffb57342de0ff03e0e1a8eca394a5
Glitch99 <scottafjarvis@gmail.com>
2019-01-26 18:14:52 +0000

    testUpdate

---
+++ UnityProject/Assets/Prefabs/Character/CharBehavior.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharBehavior : MonoBehaviour {
+
+    [SerializeField]
+    Animator charAnimator;
+    void goLeft() { }
+    void goRight() { }
+    void onHit() { }
+    void onCombo() { }
+    void startGame() { }
+    void endGame() { }
+}
---
+++ UnityProject/Assets/Scripts/buildingBounce.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class buildingBounce : MonoBehaviour
+{
+
+    [SerializeField]
+    Animator buildAnim;
+    public void bounce() {
+        buildAnim.SetTrigger("bounce");
+    }
+}
