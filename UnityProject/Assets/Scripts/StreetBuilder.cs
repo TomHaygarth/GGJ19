b783bdc0dccc6e9f162fd673a25e0b29ea115ecf
Glitch99 <scottafjarvis@gmail.com>
2019-01-26 16:15:36 +0000

    Test Scenes
    Added scenes to test code in

--- UnityProject/Assets/Scripts/StreetBuilder.cs
+++ UnityProject/Assets/Scripts/StreetBuilder.cs
@@ -7,12 +7,13 @@ public class StreetBuilder : MonoBehaviour
 
     [SerializeField]
     private GameObject[] _buildings;
+    public GameObject[] _path;
 
     GameObject streetLine(RhythmSegment rhythm)
     { // Creates buildings next to track
         GameObject streetSection = new GameObject();
 
-        for (int i = 1; i < rhythm.BeatObstacles.Length; i += 4)
+        for (int i = 0; i < rhythm.BeatObstacles.Length; i += 4)
         {
             if (rhythm.BeatObstacles[i] == RhythmSegment.ObstacleType.None)
             {
@@ -30,7 +31,7 @@ public class StreetBuilder : MonoBehaviour
             lBuild.transform.localPosition = new Vector3(-4, 0, 1 * i); ;
             lBuild.transform.Rotate(Vector3.up, 90);
         }
-        GameObject road = GameObject.Instantiate(_buildings[0]);
+        GameObject road = GameObject.Instantiate(_path[0]);
         road.transform.SetParent(streetSection.transform, false);
         road.transform.localPosition = new Vector3(0, 0, 1);
         road.transform.localScale = new Vector3(1.6f, 0.4f, 1.5f);
